4a31595eff02de58c98eb7726bbc006d9249ea30
Shiraga <matheus.s.rasera@gmail.com>
2020-06-05 16:35:08 -0300

    back to initial

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,9 +1,11 @@
-var response;
+var express = require('express');
+app = express();
 
 app.get('/', function (req, res) {
-    response = 'This is version 3 of the app.' + '\n';
-
-    //send the response to the client
-    res.send(response);
-
+  res.send('Hello World!\n');
 });
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
