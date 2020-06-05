992dc240be5228a70b0bfa27589c67da24a2f924
Shiraga <matheus.s.rasera@gmail.com>
2020-06-05 12:21:09 -0300

    updating right

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,13 +1,9 @@
-// var express = require('express');
-// app = express();
+var express = require('express');
+app = express();
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!\n');
-// });
-
-// app.listen(8080, function () {
-//   console.log('Example app listening on port 8080!');
-// });
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
 
 var response;
 
